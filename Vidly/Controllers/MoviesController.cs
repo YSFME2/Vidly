e6e961e5ed28542c956ae61243b1b9634525040f
Youssef Shaaban <Youssef Shaaban@DESKTOP-MVTT69L>
2021-04-16 15:56:42 +0200

    convention based routing

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -13,6 +13,10 @@ namespace Vidly.Controllers
         public ActionResult Index()
         {
             return View();
+            //return HttpNotFound();
+            //return Content("Youssef" );
+            //return Redirect("https://www.google.com");
+            //return File(Server.MapPath("~/El.Kanz.El.Haqiqah.wa.el.Khayal.1.2017.1080P.WEB.akwam.net.mp4"), "application/pdf","File.pdf");
         }
 
 
@@ -21,6 +25,19 @@ namespace Vidly.Controllers
         {
             var movie = new Movie() { Name = "Inception" };
             return View(movie);
+            //return new HttpNotFoundResult();
+        }
+
+        public ActionResult Par(int id)
+        {
+            return Content("ID = " + id);
+        }
+
+        public ActionResult Realased(int year, int month) => Content($"Year = {year} \nMounth = {month}");
+
+        public ActionResult Find(int year,int rate,int typed,string age,string word)
+        {
+            return Content($"year = {year} , rate = {rate/10}/10 , type = {typed} , age = {age} , word = {word}");
         }
     }
 }
